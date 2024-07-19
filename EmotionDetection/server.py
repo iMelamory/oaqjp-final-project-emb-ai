e523ddc80e8598948c0113190e8a32d0e6cb9b85
iMelamory <spassiri@gmail.com>
2024-07-19 09:55:45 -0400

    small update

--- EmotionDetection/server.py
+++ EmotionDetection/server.py
@@ -9,14 +9,19 @@ def emotion_detect():
     text_to_analyze = request.args.get('textToAnalyze')
     # Pass the text to the emotion_detector function and store the response
     response = emotion_detector(text_to_analyze)
-    
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
+    
     # Return a formatted string with the sentiment label and score
-    return "For the given statement, the system response is {}".format(response)
+    return f"For the given statement, the system response is 'anger':"f"{anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. "f"The dominant emotion is {dominant_emotion}."
 
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=5000)
